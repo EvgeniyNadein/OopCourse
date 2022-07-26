02f9e8eb9c0b1dbf3447a7768c271cdf47c0325e
EvgeniyNadein <evg-nadein@mail.ru>
2022-07-26 21:53:29 +0200

    Рабочая версия №1 класса Range

--- Range/src/ru/academits/nadein/range/Range.java
+++ Range/src/ru/academits/nadein/range/Range.java
@@ -1,5 +1,136 @@
 package ru.academits.nadein.range;
 
 public class Range {
+    private double from;
+    private double to;
 
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Range() {
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    public Range getRangeCrossover(Range object1, Range object2) {
+        Range rangeCrossover = new Range();
+
+        if (object1.getFrom() == object2.getFrom() && object1.getTo() == object2.getTo()) {
+            rangeCrossover.setFrom(object1.getFrom());
+            rangeCrossover.setTo(object1.getTo());
+
+            return rangeCrossover;
+        }
+
+        if (object2.getFrom() >= object1.getFrom() && object2.getFrom() < object1.getTo()
+                && object2.getTo() >= object1.getTo()) {
+            rangeCrossover.setFrom(object2.getFrom());
+            rangeCrossover.setTo(object1.getTo());
+
+            return rangeCrossover;
+        }
+
+        if (object1.getFrom() >= object2.getFrom() && object1.getFrom() < object2.getTo()
+                && object1.getTo() >= object2.getTo()) {
+            rangeCrossover.setFrom(object2.getFrom());
+            rangeCrossover.setTo(object2.getTo());
+
+            return rangeCrossover;
+        }
+
+        return null;
+    }
+
+    public Range[] getRangeUnion(Range object1, Range object2) {
+        Range unitedRange1 = new Range();
+        Range unitedRange2 = new Range();
+        Range[] rangeArray = {null, null};
+
+        if (object1.getFrom() == object2.getFrom() && object1.getTo() == object2.getTo()) {
+            unitedRange1.setFrom(object1.getFrom());
+            unitedRange1.setTo(object1.getTo());
+            rangeArray[0] = unitedRange1;
+        } else if (object2.getFrom() >= object1.getFrom() && object2.getFrom() <= object1.getTo()
+                && object2.getTo() >= object1.getTo()) {
+            unitedRange1.setFrom(object1.getFrom());
+            unitedRange1.setTo(object2.getTo());
+            rangeArray[0] = unitedRange1;
+        } else if (object1.getFrom() >= object2.getFrom() && object1.getFrom() <= object2.getTo()
+                && object1.getTo() >= object2.getTo()) {
+            unitedRange1.setFrom(object2.getFrom());
+            unitedRange1.setTo(object1.getTo());
+            rangeArray[0] = unitedRange1;
+        } else {
+            unitedRange1.setFrom(object1.getFrom());
+            unitedRange1.setTo(object1.getTo());
+
+            unitedRange2.setFrom(object2.getFrom());
+            unitedRange2.setTo(object2.getTo());
+
+            rangeArray[0] = unitedRange1;
+            rangeArray[1] = unitedRange2;
+        }
+
+        return rangeArray;
+    }
+
+    public Range[] getRangeDifference(Range object1, Range object2) {
+        Range rangeDifference1 = new Range();
+        Range rangeDifference2 = new Range();
+        Range[] rangeArray = {null, null};
+
+        if (object2.getFrom() > object1.getFrom() && object2.getFrom() < object1.getTo()
+                && object2.getTo() >= object1.getTo()) {
+            rangeDifference1.setFrom(object1.getFrom());
+            rangeDifference1.setTo(object2.getFrom());
+            rangeArray[0] = rangeDifference1;
+        } else if (object1.getFrom() > object2.getFrom() && object1.getFrom() <= object2.getTo()
+                && object1.getTo() >= object2.getTo()) {
+            rangeDifference1.setFrom(object2.getFrom());
+            rangeDifference1.setTo(object1.getFrom());
+            rangeArray[0] = rangeDifference1;
+        } else if (object1.getFrom() > object2.getFrom() && object1.getTo() > object2.getTo()) {
+            rangeDifference1.setFrom(object1.getFrom());
+            rangeDifference1.setTo(object2.getFrom());
+            rangeArray[0] = rangeDifference1;
+
+            rangeDifference2.setFrom(object2.getTo());
+            rangeDifference2.setTo(object1.getTo());
+            rangeArray[1] = rangeDifference2;
+        } else if (object1.getFrom() > object2.getFrom() && object1.getTo() < object2.getTo()) {
+            rangeDifference1.setFrom(object2.getFrom());
+            rangeDifference1.setTo(object1.getFrom());
+            rangeArray[0] = rangeDifference1;
+
+            rangeDifference2.setFrom(object1.getTo());
+            rangeDifference2.setTo(object2.getTo());
+            rangeArray[1] = rangeDifference2;
+        }
+
+        return rangeArray;
+    }
 }
--- Range/src/ru/academits/nadein/range_main/Main.java
+++ Range/src/ru/academits/nadein/range_main/Main.java
@@ -1,4 +1,72 @@
 package ru.academits.nadein.range_main;
 
+import ru.academits.nadein.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Range range1 = new Range(1, 100);
+        Range range2 = new Range(100, 200);
+
+        if (range1.getRangeCrossover(range1, range2) != null) {
+            System.out.println("Начало диапазона пересечения = " + range1.getRangeCrossover(range1, range2).getFrom());
+            System.out.println("Конец диапазона пересечения = " + range1.getRangeCrossover(range1, range2).getTo());
+        } else {
+            System.out.println("Интервал пересечения = " + range1.getRangeCrossover(range1, range2));
+        }
+
+        System.out.println();
+
+        if (range1.getRangeUnion(range1, range2)[1] != null) {
+            System.out.println("Начало объединенного интервала1 = " + range1.getRangeUnion(range1, range2)[0].getFrom());
+            System.out.println("Конец объединенного интервала1 = " + range1.getRangeUnion(range1, range2)[0].getTo());
+
+            System.out.println("Начало объединенного интервала2 = " + range1.getRangeUnion(range1, range2)[1].getFrom());
+            System.out.println("Конец объединенного интервала2 = " + range1.getRangeUnion(range1, range2)[1].getTo());
+        } else {
+            System.out.println("Начало объединенного интервала = " + range1.getRangeUnion(range1, range2)[0].getFrom());
+            System.out.println("Конец объединенного интервала = " + range1.getRangeUnion(range1, range2)[0].getTo());
+        }
+
+        System.out.println();
+
+        if (range1.getRangeDifference(range1, range2)[1] != null) {
+            System.out.println("Начало 1 разности интервалов = " + range1.getRangeDifference(range1, range2)[0].getFrom());
+            System.out.println("Конец 1 разности интервалов = " + range1.getRangeDifference(range1, range2)[0].getTo());
+
+            System.out.println("Начало 2 разности интервалов = " + range1.getRangeDifference(range1, range2)[1].getFrom());
+            System.out.println("Конец 2 разности интервалов = " + range1.getRangeDifference(range1, range2)[1].getTo());
+        } else if (range1.getRangeDifference(range1, range2)[0] != null){
+            System.out.println("Начало разности интервалов = " + range1.getRangeDifference(range1, range2)[0].getFrom());
+            System.out.println("Конец разности интервалов = " + range1.getRangeDifference(range1, range2)[0].getTo());
+        } else {
+            System.out.println("Разность интервалов = " + range1.getRangeDifference(range1, range2)[0]);
+        }
+
+        System.out.println();
+
+        System.out.println("Начало диапазона = " + range1.getFrom());
+        System.out.println("Конец диапазона = " + range1.getTo());
+        System.out.println("Длина диапазона = " + range1.getLength());
+
+        System.out.println("Введите число для определения принадлежности к диапазону:");
+        double number1 = scanner.nextDouble();
+
+        System.out.println("Число " + number1 + " принадлежит диапазону: " + range1.isInside(number1));
+        System.out.println();
+
+        range1.setFrom(20);
+        range1.setTo(80);
+
+        System.out.println("Начало диапазона = " + range1.getFrom());
+        System.out.println("Конец диапазона = " + range1.getTo());
+        System.out.println("Длина диапазона = " + range1.getLength());
+
+        System.out.println("Введите число для определения принадлежности к диапазону:");
+        double number2 = scanner.nextDouble();
+
+        System.out.println("Число " + number2 + " принадлежит диапазону: " + range1.isInside(number2));
+    }
 }
