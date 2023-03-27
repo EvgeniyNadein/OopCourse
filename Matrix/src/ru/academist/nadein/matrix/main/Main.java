b83bcd055324aa4d7f9193e3172d0c4c4c0dd643
EvgeniyNadein <evg-nadein@mail.ru>
2023-03-27 17:05:11 +0300

    matrix - правки

--- Matrix/src/ru/academist/nadein/matrix/main/Main.java
+++ Matrix/src/ru/academist/nadein/matrix/main/Main.java
@@ -37,24 +37,33 @@ public class Main {
         System.out.println(matrix2);
         System.out.println();
 
-        double[][] inputArray3 = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
-        double[][] inputArray4 = {{1, 2, 3}, {1, 1, 1}, {0, 0, 0}, {2, 1, 0}};
-        Matrix matrix3 = new Matrix(inputArray3);
-        Matrix matrix4 = new Matrix(inputArray4);
+        double[][] array3 = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
+        double[][] array4 = {{1, 2, 3}, {1, 1, 1}, {0, 0, 0}, {2, 1, 0}};
+        Matrix matrix3 = new Matrix(array3);
+        Matrix matrix4 = new Matrix(array4);
         System.out.println(matrix3);
         System.out.println(matrix4);
         System.out.println("Умножение матриц: " + Matrix.getProduct(matrix3, matrix4));
         System.out.println();
 
-        double[][] inputArray5 = {{1, 2, 2}, {3, 1, 1}};
-        double[][] inputArray6 = {{4, 2}, {3, 1}, {1, 5}};
-        Matrix matrix5 = new Matrix(inputArray5);
-        Matrix matrix6 = new Matrix(inputArray6);
+        double[][] array5 = {{1, 2, 2}, {3, 1, 1}};
+        double[][] array6 = {{4, 2}, {3, 1}, {1, 5}};
+        Matrix matrix5 = new Matrix(array5);
+        Matrix matrix6 = new Matrix(array6);
         System.out.println(matrix5);
         System.out.println(matrix6);
         matrix4.setRowByIndex(2, vector2);
         System.out.println(matrix6);
         System.out.println("Умножение матриц: " + Matrix.getProduct(matrix6, matrix5));
+        System.out.println();
+
+        double[][] array7 = {{5, 2,}, {3, 1}};
+        double[][] array8 = {{4, 6}, {5, 2}};
+        Matrix matrix7 = new Matrix(array7);
+        Matrix matrix8 = new Matrix(array8);
+        System.out.println(matrix7);
+        System.out.println(matrix8);
+        System.out.println("Умножение матриц: " + Matrix.getProduct(matrix7, matrix8));
         System.out.println();
 
         matrix6.transpose();
