ff0bd0b968c40ac97b05989809663b9dbb5b7917
EvgeniyNadein <evg-nadein@mail.ru>
2023-04-10 15:32:13 +0300

    csv - правки

--- CSV/src/ru/academits/nadein/csv/Main.java
+++ CSV/src/ru/academits/nadein/csv/Main.java
@@ -1,19 +1,12 @@
 package ru.academits.nadein.csv;
 
 import java.io.*;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("CSV/src/zadachaCSV2.csv"));
-             PrintWriter writer = new PrintWriter("FileInHtml.html")) {
-
-            String currentLine = bufferedReader.readLine();
-
-            while (currentLine != null) {
-                currentLine = bufferedReader.readLine();
-
-            }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\OopCourse\\ZadachaCSV.csv", StandardCharsets.UTF_8));
+             PrintWriter writer = new PrintWriter("FileInHtml.html", StandardCharsets.UTF_8)) {
 
             writer.println("<!DOCTYPE HTML>");
             writer.println("<html>");
@@ -24,91 +17,108 @@ public class Main {
             writer.println("<body>");
             writer.println("<table border=\"1\" width=\"10\" height=\"10\">");
 
-            boolean isLineBreak = false;
+            String currentLine = bufferedReader.readLine();
+            boolean isTableDetail = false;
 
-            for (int j = 0; j < linesFromFile.length; j++) {
-                if (j == 0) {
-                    writer.println("<tr>");
-                    writer.println("<td>");
+            while (currentLine != null) {
+                if (currentLine.isBlank()) {
+                    currentLine = bufferedReader.readLine();
+
+                    continue;
                 }
 
-                if (j > 0 && !isLineBreak) {
+                if (!isTableDetail) {
                     writer.println("<tr>");
-                    writer.println("<td>");
+                    writer.println("    <td>");
                 }
 
-                for (int k = 0; k < linesFromFile[j].length() - 2; k++) {
-                    char symbol = linesFromFile[j].charAt(k);
-                    char nextSymbol = linesFromFile[j].charAt(k + 1);
-                    char thirdSymbol = linesFromFile[j].charAt(k + 2);
+                for (int i = 0; i < currentLine.length() - 1; i++) {
+                    char symbol = currentLine.charAt(i);
+                    char nextSymbol = currentLine.charAt(i + 1);
 
-                    if (symbol == ',' && nextSymbol != '"' && thirdSymbol != ',') {
-                        writer.println("</td>");
-                        writer.println("<td>");
-                    }
+                    if (symbol == ',' && nextSymbol == '"' && !isTableDetail) {
+                        isTableDetail = true;
+                        writer.println();
+                        writer.println("    </td>");
+                        writer.println("    <td>");
+                        i++;
 
-                    if (symbol == ',' && nextSymbol == '"' && thirdSymbol == ',') {
-                        writer.println(symbol);
-                        isLineBreak = false;
-                        writer.println("</td>");
-                        writer.println("<td>");
-                        continue;
-                    }
-
-                    if (symbol == ',' && nextSymbol == '"') {
-                        isLineBreak = true;
-                        writer.println("</td>");
-                        writer.println("<td>");
-                        k++;
-                        continue;
-                    }
-
-                    if (symbol == '"' && nextSymbol == '"') {
-                        writer.print('"');
-                        k++;
                         continue;
                     }
 
                     if (symbol == '"' && nextSymbol == ',') {
-                        writer.println("</td>");
-                        writer.println("<td>");
-                        isLineBreak = false;
-                        k++;
+                        isTableDetail = false;
+                        writer.println();
+                        writer.println("    </td>");
+                        writer.println("    <td>");
+                        i++;
+
                         continue;
                     }
 
-                    if (symbol == ',' && isLineBreak) {
-                        writer.print(symbol);
-                        k++;
+                    if (i == currentLine.length() - 2 && symbol == '"' && nextSymbol == '"') {
+                        writer.println();
+                        writer.println("    </td>");
+
+                        continue;
                     }
 
-                    if (k == linesFromFile[j].length() - 3 && thirdSymbol != ',' && !isLineBreak) {
+                    if (symbol == '"' && nextSymbol == '"') {
+                        writer.print(symbol);
+                        i++;
+
+                        continue;
+                    }
+
+                    if (nextSymbol == ',' && i == currentLine.length() - 2) {
+                        writer.println();
+                        writer.println("    </td>");
+                        writer.println("    <td>");
+                        writer.println("    </td>");
+                        writer.println("</tr>");
+                    }
+
+                    if (symbol == ',' && nextSymbol != '"') {
+                        writer.println();
+                        writer.println("    </td>");
+                        writer.println("    <td>");
+
+                        continue;
+                    }
+
+                    if (i == currentLine.length() - 2 && nextSymbol != ',' && isTableDetail) {
                         writer.print(symbol);
                         writer.print(nextSymbol);
-                        writer.println(thirdSymbol);
-                        writer.println("</td>");
+                        writer.print("<br>");
+                        writer.println();
+
                         continue;
                     }
 
                     writer.print(symbol);
-                }
 
-                if (isLineBreak) {
-                    writer.print("<br>");
-                } else {
+                    if (i == currentLine.length() - 2 && nextSymbol != ',' && !isTableDetail) {
+                        writer.print(nextSymbol);
+                        writer.println();
+                        writer.println("    </td>");
+                    }
+                }
+                if (!isTableDetail) {
+                    writer.println();
                     writer.println("</tr>");
                 }
-            }
 
+                currentLine = bufferedReader.readLine();
+            }
             writer.println("</table>");
             writer.println("</body>");
             writer.println("</html>");
+
 
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IOException e) {
             System.out.println("Что то пошло не так, проверьте файл.");
         }
-    } // т.к получился говнокод (сам вижу)), хотя казалось норм идея, есть вариант переписать с ArrayList строк
-//  и использовать строковые функции например replace() (нужно подсказать).
+    }
 }
